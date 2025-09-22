8f1aa460c789e0e7d9ecb099bf80521373170cad
douglas rocha <douglashidekazu15@gmail.com>
2025-09-22 16:38:21 +0900

    added counter, learning concept of state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css';
 import Header from './Header'
+import Counter from "./Counter";
 
 function App() {
   return(
-    <Header name="DougDev" links = {["About", "Buy", "Contact", "Login"]}></Header>
+    <div>
+      <Header name="DougDev" links = {["About", "Buy", "Contact", "Login"]}></Header>
+      <Counter count={3}></Counter>
+    </div>
   )
 }
 
---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Counter extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { count: this.props.count }
+    this.add = this.add.bind(this)
+  }
+
+  add() {
+    // ---- bad practice ----
+    // this.setState({ count: this.state.count + 1} )
+    // ---- good practice(function to update state) ----
+    this.setState((state)=>{return {count: state.count + 1}})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter:{this.state.count}</h1>
+        <button onClick = {this.add}>Add</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
